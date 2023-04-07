7d552f3aacf13986c47b436f512f7b1104ced963
Denis Son <denis@air-de-denis.home>
2023-04-07 14:14:52 +0200

    first

--- routes/trip.js
+++ routes/trip.js
@@ -15,7 +15,7 @@ router.get("user/trip/:id", isAuthenticated, async (req, res) => {
       select: "account",
     });
 
-    console.log(trip, "hello");
+    console.log(trip, "helldqdzo");
     res.status(200).json({ trip: trip });
   } catch (error) {
     res.status(400).json({ message: error.message });
